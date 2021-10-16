dbca6c6205b6ab7cae7c08d7164448d5364a07fc
Ryan McClain <r10ngw@gmail.com>
2021-10-16 01:35:29 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+namespace TagTree
+{
+    class Program  //application 
+    {
+
+        public static string getString()   //gets string from user
+        {
+            Console.WriteLine("Enter palindrome:");
+            return Console.ReadLine();
+        }
+        public static void printer(string finalString)   //prints to file and writes output in terminal 
+        {
+            string outFile = @"./output.txt";
+            File.WriteAllText(outFile, finalString);
+            Console.Write(finalString);
+        }
+
+        public static string palindromeCheck(string inputString)   //checks if user input is palindrome and returns result 
+        {
+            string reverse = "";
+            for (int i = inputString.Length - 1; i >= 0; i--) //assigns new variable the characters of inpout string in reverse order
+                reverse += inputString[i];
+            if (reverse == inputString)   //checks if it is a palindrome
+                return (reverse + " is Palindrome");
+            else
+                return (reverse + " isnt palindrome");
+        }
+        
+        public static void Main(string[] args)  //main string in class
+        {
+            string input = getString();    //gets input
+            printer(palindromeCheck(input));  //checks if string is a palindrome and prints it to a file and terminal 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Tree Relationship builder
+ Version 1.0.0
+
+
+ Contribuors
+    Ryan McClain
